fcdb9f5861f55f5a41a3bf7f6ba23b40239de627
Ubuntu <ubuntu@ip-172-31-30-217.us-east-2.compute.internal>
2021-11-10 14:34:13 +0000

    fix

--- train.py
+++ train.py
@@ -191,7 +191,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
         del ckpt, csd
 
     # Image sizes
-    gs = min(int(model.stride.max()), 32)  # grid size (max stride)
+    gs = max(int(model.stride.max()), 32)  # grid size (max stride)
     nl = model.model[-1].nl  # number of detection layers (used for scaling hyp['obj'])
     imgsz = check_img_size(opt.imgsz, gs, floor=gs * 2)  # verify imgsz is gs-multiple
 
