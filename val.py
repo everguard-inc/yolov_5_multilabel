10c13c919fa2d54aef92559da916f31adfa5ae81
Rodion Bukhanevych <50274168+RodionBukhanevych11@users.noreply.github.com>
2021-11-16 13:57:42 +0200

    Update val.py

--- val.py
+++ val.py
@@ -77,8 +77,8 @@ def run(data,
         weights=None,  # model.pt path(s)
         batch_size=32,  # batch size
         imgsz=640,  # inference size (pixels)
-        conf_thres=0.4,  # confidence threshold
-        iou_thres=0.4,  # NMS IoU threshold
+        conf_thres=0.01,  # confidence threshold
+        iou_thres=0.45,  # NMS IoU threshold
         task='val',  # train, val, test, speed or study
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         single_cls=False,  # treat as single-class dataset
